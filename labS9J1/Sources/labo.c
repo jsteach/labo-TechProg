bad70cecf764e4d67599dc2c716ff4f90fc1df26
Olivier <olivierclermontf@gmail.com>
2025-04-06 08:12:23 -0400

    MAJ Dimanche 6 Avril - S9J1 : Bloquer step 3 DFS. S9J2 : Trouble de gestion memoire.

--- labS9J1/Sources/labo.c
+++ labS9J1/Sources/labo.c
@@ -1,16 +1,16 @@
 ﻿#include "labo.h"
 
 /*
-* Creer un Noeud, assigner lui la valeur de data, faite un memset 0 sur adj, visited a faux, len a 0, 
+* Creer un Noeud, assigner lui la valeur de data, faite un memset 0 sur adj, visited a faux, len a 0,
 allouer de la memoire pour le QNode et assurer vous de mettre la propiete prev de QNode a NULL.
 * Retourner ensuite le pointeur vers le Node.
 */
-Node* create_node(void* data){
+Node* create_node(void* data) {
 	Node* newnode = (Node*)allocate(sizeof(Node));
 	newnode->data = data;
 	newnode->visited = 0;
 	newnode->len = 0;
-	memset(newnode->adj, 0, sizeof(Node)*UINT8_MAX);
+	memset(newnode->adj, 0, sizeof(Node) * UINT8_MAX);
 
 	QNode* Qnode = (QNode*)allocate(sizeof(QNode));
 	Qnode->next = NULL;
@@ -22,7 +22,7 @@ Node* create_node(void* data){
 /*
 * Ajouter le node dans la liste d'adjacence de root.
 */
-void add_adjacent_node(Node* root, Node* node){
+void add_adjacent_node(Node* root, Node* node) {
 	root->adj[root->len++] = node;
 	//for (int i = 0; i <= root->adjroot->adj[i] = node;i++) { // Ca ici marche mais O(n)
 	//	if (root->adj[i]==NULL){
@@ -33,20 +33,40 @@ void add_adjacent_node(Node* root, Node* node){
 }
 
 /*
-* Programmer l'algorithme de depth first search afin de trouver la cle. 
+* Programmer l'algorithme de depth first search afin de trouver la cle.
 * Pour programmer cet algorithme, vous avez besoins d'une pile/stack, ca definition est fourni.
 * La Stack devrait contenir la liste en ordre inverse de celle parcouru.
 * i.e. si le chemin est A -> B -> C la stack avec son pop devrait retourner C -> B -> A
 */
-int dfs(Node* root[], int len, Node* curr, void* key, Stack* s){
-	Stack s = stack_init(16);
+int dfs(Node* root[], int len, Node* curr, void* key, Stack* s) {
+for (int i = 0; i < len; i++) {
+		Node* adj = ((Node*)root[i]->adj)->data;
 
-	stack_push(s, curr);
+		if (root[i]->data != key && adj->data != key) {
+			if (adj->visited == 0) {
+				root[i]->visited = 1;
+				adj->visited = 1;
+				stack_push(s, root[i]);
+			}
+		}
 
+		if (root[i]->data == key) {
+			stack_push(s, root[i]);
+			return 0;
+		}
+		if (adj->data == key) {
+			adj->visited = 1;
+			stack_push(s, root[i]);
+			stack_push(s, adj);
+			return 0;
+		}
+		root[i]->visited = 1;
+	}
+	return 0;
 }
 
 /*
 * Programmer l'algorithme de breath first search afin de trouver la cle. Pour programmer cet algorithme, vous avez besoins d'une file/queue, ca definition est fourni.
 * La Stack devrait contenir la liste en ordre du chemin parcouru. i.e. si le chemin est A -> B -> C la stack avec son pop devrait retourner A -> B -> C
 */
-int bfs(Node* root[], void* key, Stack* s){}
+int bfs(Node* root[], void* key, Stack* s) {}
--- labS9J2/Sources/labo.c
+++ labS9J2/Sources/labo.c
@@ -1,1 +1,50 @@
 #include "labo.h"
+
+/*
+* Creer une matrice d'adjacence. Mettre len a 0. Mettre max_size a max_nodes.
+* Allouer de la memoire pour le max de noeud specifier i.e. pour graph->nodes.
+* Pour chaque noeud de graph->nodes mettre le cost et path_from a UINT8_MAX et visited a 0.
+* 
+* Pour adjGraph, allouer une quantiter de int* relatif a la valeur de max_nodes.
+* Dans chaque int* allouer un tableau de int relatif a la valeur max_nodes.
+* A chaque index de adjGraph mettez son cout a 0.
+* 
+* 
+* Retourner le graph creer.
+*/
+AdjMatrix* create_graph(size_t max_nodes){
+	AdjMatrix* newMatrix;
+	newMatrix = (AdjMatrix*)allocate(sizeof(AdjMatrix));
+	newMatrix->len = 0;
+	newMatrix->max_size = max_nodes;
+	newMatrix->nodes = (Node*)allocate(sizeof(Node));
+	newMatrix->nodes->cost = UINT8_MAX;
+	newMatrix->nodes->path_from = UINT8_MAX;
+	newMatrix->nodes->visited = 0;
+
+	newMatrix->adjGraph = (int**)allocate(sizeof(int*));
+	for (int i = 0; i < max_nodes;i++) {
+		for (int j = 0; j < max_nodes;j++) {
+			((Node*)newMatrix->adjGraph[i][j])->cost = 0;
+		}
+	}
+	
+
+
+	return newMatrix;
+}
+
+/*
+* Creer un node, lui attribuer le data et l'ajouter dans la matrice d'adjacence.
+*/
+void add_node(AdjMatrix* graph, void* data){}
+
+/*
+* Ajouter un lien dans la matrice d'adjacence, d'un noeud a l'autre noeud, en specifiant le cout y etant relier.
+*/
+void add_edge(AdjMatrix* graph, int fromNode, int toNode, uint8_t cost){}
+
+/*
+* Implementer l'algorithme de dijkstra.
+*/
+void dijkstra(AdjMatrix* graph, int startNodeIndex, int endNodeIndex, Stack* solvedPath){}
