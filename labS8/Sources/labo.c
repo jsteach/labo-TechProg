ec716cad02268fbe9e577262debf1be2c2d322d9
Gabriel Côté <2430016@bart.ca>
2025-04-24 09:52:19 -0400

    remise lab prog

--- labS8/Sources/labo.c
+++ labS8/Sources/labo.c
@@ -1,1 +1,64 @@
 #include "labo.h"
+/*
+* Programmer l'algorithme de fibonacci. Commencer par faire l'algorithme de base et par la suite ajouter la memoization.
+* Faire attention lorsque vous utilisez un type pour une variable qui represente un nombre, int ne depasse pas 2^31-1 ou 2,147,483,647
+*/
+long long int Memory[75] = { 0 };
+long long fibonacci(int n) {
+	size_t fibonacci(size_t n); {
+		if (n <= 1) return n;
+		if (Memory[n] == NULL) Memory[n] = fibonacci(n - 1) + fibonacci(n - 2);
+		if (Memory[n - 1] != NULL) {
+			return Memory[n - 1] + fibonacci(n - 2);
+			Memory[n] = Memory[n - 1] + fibonacci(n - 2);
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+		
+	
+}
+/*
+* Creer un Noeud, assigner lui la valeur de data, metter c'est reference a NULL et retourner le noeud.
+*/
+TreeNode* tree_create_node(void* data) {
+	TreeNode* Node = allocate(sizeof(TreeNode));
+	Node->left = NULL;
+	Node->right = NULL;
+	return Node;
+}
+/*
+* Ajouter le noeud  node dans la propriete left ou right qui est NULL de root. On priorise de gauche a droite. Si right et left son NULL on le met dans left.
+* Si jamais aucun des noeuds est NULL, ajouter rien.
+*/
+void tree_add_node(TreeNode* root, TreeNode* node) {
+	if (root->left == NULL && root->right == NULL) {
+		root->left = node;
+	}
+	if (root->left != NULL && root->right == NULL) {
+		root->right = node;
+	}
+	if (root->left == NULL && root->right != NULL) {
+		root->left = node;
+	}
+	if (root->left != NULL && root->right != NULL) {
+		return;
+	}
+
+}
+
+/*
+* Programmer l'algorithme de depth first search afin de trouver la cle. Pour programmer cet algorithme, vous avez besoins d'une pile/stack, ca definition est fourni.
+*/
+int dfs(TreeNode* root, void* key) {
+	
+
+	
+
+}
+
+/*
+* Programmer l'algorithme de breath first search afin de trouver la cle. Pour programmer cet algorithme, vous avez besoins d'une file/queue, ca definition est fourni.
+*/
+int bfs(TreeNode* root, void* key) {
+	
+}
--- labS9J1/Sources/labo.c
+++ labS9J1/Sources/labo.c
@@ -1,1 +1,104 @@
 ﻿#include "labo.h"
+Node* create_node(void* data) {
+	Node* newNode = (Node*)allocate(sizeof(Node));
+	(char*)newNode->data = (char*)data;
+	memset(newNode->adj, 0, sizeof(newNode->adj));
+	newNode->visited = 0;
+	newNode->len = 0;
+	newNode->revPath = (QNode*)allocate(sizeof(QNode));
+	newNode->revPath->data = newNode;
+	newNode->revPath->prev = NULL;
+	return newNode;
+};
+
+
+void add_adjacent_node(Node* root, Node* node) {
+	root->adj[root->len] = node;
+	root->len++;
+};
+
+
+int dfs(Node* root[], int len, Node* curr, void* key, Stack* s) {
+	int VisitCount = 0;
+
+	if (curr == NULL) {
+		curr = root[0];
+	}
+	stack_push(s, curr); 
+	curr->visited = 1;
+
+	while (s->top > -1) {
+		Node* PopNode = stack_pop(s);
+		curr = PopNode;
+		VisitCount++;
+
+		if (curr->data == key) {
+			while (s->top > -1) stack_pop(s);
+
+			Stack tempStack = stack_init(100);
+			stack_push(&tempStack, curr);
+
+			while (curr->revPath != NULL && curr->revPath->prev != NULL) {
+				curr = curr->revPath->prev->data;
+				stack_push(&tempStack, curr);
+			}
+
+			while (tempStack.top > -1) {
+				stack_push(s, stack_pop(&tempStack));
+			}
+
+			return VisitCount;
+		}
+		for (int i = 0; i < curr->len; i++) {
+			if (curr->adj[i]->visited == 0) {
+				curr->adj[i]->visited = 1;
+				curr->adj[i]->revPath->prev = curr->revPath;
+				stack_push(s, curr->adj[i]);
+			}
+		} 
+
+	}
+	return -1;
+};
+
+int bfs(Node* root[], void* key, Stack* s) {
+	int VisitCount = 0;
+	Queue Queue;
+	queue_init(&Queue);
+
+	Node* curr = root[0];
+	curr->visited = 1;
+	curr->revPath->data = curr; 
+	curr->revPath->prev = NULL;
+	queue_push(&Queue, curr);
+
+	while (Queue.count > 0) {
+		curr = queue_pop(&Queue);
+		VisitCount++;
+
+		if (curr->data == key) {
+			stack_push(s, curr);
+
+			while (curr->revPath && curr->revPath->prev) {
+				curr = curr->revPath->prev->data;
+				stack_push(s, curr);
+			}
+
+			return VisitCount;
+		}
+
+		for (int i = 0; i < curr->len; i++) {
+			Node* adjnode = curr->adj[i];
+			if (adjnode->visited == 0) {
+				adjnode->visited = 1;
+
+				adjnode->revPath->data = adjnode;
+				adjnode->revPath->prev = curr->revPath;
+
+				queue_push(&Queue, adjnode);
+			}
+		}
+	}
+
+	return VisitCount;
+}
